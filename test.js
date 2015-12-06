8553c00e5e8faed90da70a87686a3a52604a3e42
i5ting <i5ting@126.com>
2015-12-06 22:55:32 +0800

    refact

--- test.js
+++ test.js
@@ -1,29 +1,14 @@
 var app = require('koa')();
-var koa = require('koa-router')(); 
-
 
 var mount = require('./');
 
+// simple
+// mount(app);
+// with path
+// mount(app, __dirname + '/routes2');
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-});
-
-
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-//
-// // routes definition
-// koa.use('/', index.routes(), index.allowedMethods());
-// koa.use('/users', users.routes(), users.allowedMethods());
-
-// mount root routes  
-// app.use(koa.routes());
-
-mount(app,'./routes', true);
+// with path & api dump
+mount(app,  __dirname + '/routes', true);
 
 app.on('error', function(err, ctx){
   log.error('server error', err, ctx);
